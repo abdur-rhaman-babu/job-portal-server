16076a6eff7127ede833c80a3444a4d470824860
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-12 15:44:20 +0600

    get spacific data for showing details

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-require('dotenv').config()
-const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3000
+require("dotenv").config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -17,7 +16,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,13 +28,22 @@ async function run() {
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     // job related apis
-    const jobsCollection = client.db('job_portal').collection('jobs')
+    const jobsCollection = client.db("job_portal").collection("jobs");
 
-    app.get('/jobs', async (req, res)=>{
-        const cursor = jobsCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+    // get data for display data
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -43,11 +51,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("This server is job portal server");
+});
 
-app.get('/', (req, res)=>{
-    res.send('This server is job portal server')
-})
-
-app.listen(port, ()=>{
-    console.log(`Job server running on: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Job server running on: ${port}`);
+});
