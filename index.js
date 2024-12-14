49de4fce83fd5641029ba2c167cfc56addb247ba
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-14 11:19:00 +0600

    get specific data for showing details

--- index.js
+++ index.js
@@ -21,34 +21,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
     // job related apis
     const jobsCollection = client.db("job_portal").collection("jobs");
-    const jobApplicationCollection = client.db('job_portal').collection('job_applications')
-
-    // get data for display data
-    app.get("/jobs", async (req, res) => {
-      const cursor = jobsCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-
-    app.get("/jobs/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await jobsCollection.findOne(query);
-      res.send(result);
-    });
-
-    // post for job application
-    app.post('/job-applications', async(req, res)=>{
-      const application = req.body;
-      const result = await jobApplicationCollection.insertOne(application)
+    
+    // get data from server for display
+    app.get('/jobs', async (req, res)=>{
+      const result = await jobsCollection.find().toArray()
       res.send(result)
     })
+
+    app.get('/jobs/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query)
+      res.send(result)
+    })
+
+   
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+   
   }
 }
 run().catch(console.dir);
