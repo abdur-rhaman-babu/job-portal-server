9309735485ae32fe3a252ae399db29097e41be8a
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-14 22:18:59 +0600

    create post apis

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ async function run() {
     const applicationCollection = client.db('job_portal').collection('job_applications')
     
     // get data from server for display
+    app.post('/jobs', async (req, res)=>{
+        const newJob = req.body;
+        const result = await jobsCollection.insertOne(newJob)
+        res.send(result)
+    })
+
     app.get('/jobs', async (req, res)=>{
       const result = await jobsCollection.find().toArray()
       res.send(result)
@@ -44,6 +50,7 @@ async function run() {
         res.send(result)
     })
 
+    // get data to spacific email
     app.get('/job-application', async (req, res)=>{
       const email = req.query.email;
       const query = {application_email: email}
@@ -51,7 +58,7 @@ async function run() {
 
       // get data to another collection fokira way
       for(const application of result){
-        console.log(application.job_id)
+        // console.log(application.job_id)
         const query1 = {_id: new ObjectId(application.job_id)}
         const job = await jobsCollection.findOne(query1)
         if(job){
