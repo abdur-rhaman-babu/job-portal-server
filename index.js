879b48a3b902b7e2168559ed841c0961e112fbf8
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-12-16 22:51:10 +0600

    verify token

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 require("dotenv").config();
 const express = require("express");
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken')
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true 
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+
+const verifyToken = (req, res, next) => {
+  console.log("Inside the verify token middleware");
+  const token = req?.cookies?.token
+  if(!token){
+    return res.status(401).send({message: 'Unauthorized access'})
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err,decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'Unauthorized access'})
+    }
+    req.user = decoded
+    next()
+  })
+ 
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -29,91 +48,96 @@ async function run() {
   try {
     // job related apis
     const jobsCollection = client.db("job_portal").collection("jobs");
-    const applicationCollection = client.db('job_portal').collection('job_applications')
+    const applicationCollection = client
+      .db("job_portal")
+      .collection("job_applications");
 
     // Auth related APIs
-    app.post('/jwt', async (req, res)=>{
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1h'})
-      res.cookie('token', token,{
-        httpOnly: true,
-        secure: false,
-      })
-      .send({success: true})  
-    })
-    
-    // get data from server for display
-    app.post('/jobs', async (req, res)=>{
-        const newJob = req.body;
-        const result = await jobsCollection.insertOne(newJob)
-        res.send(result)
-    })
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
 
-    app.get('/jobs', async (req, res)=>{
+    // get data from server for display
+    app.post("/jobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result);
+    });
+
+    app.get("/jobs", async (req, res) => {
       const email = req.query.email;
       let query = {};
-      if(email){
-        query = {hr_email: email}
+      if (email) {
+        query = { hr_email: email };
       }
-      const result = await jobsCollection.find(query).toArray()
-      res.send(result)
-    })
+      const result = await jobsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.get('/jobs/:id', async (req, res)=>{
+    app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await jobsCollection.findOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.get('/job-applications/jobs/:job_id', async (req, res)=>{
-        const id = req.params.job_id;
-        const query = {job_id: id}
-        const result = await applicationCollection.find(query).toArray()
-        res.send(result)
-    })
+    app.get("/job-applications/jobs/:job_id", async (req, res) => {
+      const id = req.params.job_id;
+      const query = { job_id: id };
+      const result = await applicationCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.post('/job-applications', async (req, res)=>{
-        const application = req.body;
-        const result = await applicationCollection.insertOne(application)
-        res.send(result)
-    })
+    app.post("/job-applications", async (req, res) => {
+      const application = req.body;
+      const result = await applicationCollection.insertOne(application);
+      res.send(result);
+    });
 
-    app.patch('/job-applications/:id', async (req, res)=>{
-        const id = req.params.id;
-        const data = req.body;
-        const filter = {_id: new ObjectId(id)}
-        const updatedDoc = {
-          $set:{
-            status: data.status
-          }
-        }
-        const result = await applicationCollection.updateOne(filter, updatedDoc)
-        res.send(result)
-    })
+    app.patch("/job-applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await applicationCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     // get data to spacific email
-    app.get('/job-application', async (req, res)=>{
+    app.get("/job-application", verifyToken, async (req, res) => {
       const email = req.query.email;
-      const query = {application_email: email}
-      console.log('kuk kuk',req.cookies)
-      const result = await applicationCollection.find(query).toArray()
+      const query = { application_email: email };
+
+      if(req.user.email !== req.query.email){
+          return res.status(403).send({message:'forbidden'})
+      }
+
+      const result = await applicationCollection.find(query).toArray();
 
       // get data to another collection fokira way
-      for(const application of result){
+      for (const application of result) {
         // console.log(application.job_id)
-        const query1 = {_id: new ObjectId(application.job_id)}
-        const job = await jobsCollection.findOne(query1)
-        if(job){
+        const query1 = { _id: new ObjectId(application.job_id) };
+        const job = await jobsCollection.findOne(query1);
+        if (job) {
           application.title = job.title;
-          application.company = job.company
+          application.company = job.company;
         }
       }
-      res.send(result)
-    })
-   
+      res.send(result);
+    });
   } finally {
-   
   }
 }
 run().catch(console.dir);
